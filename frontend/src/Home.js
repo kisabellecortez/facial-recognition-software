604403a294d6b8a7cc00c0c554efbdf162f381da
kisabellecortez <kisabellecortez@gmail.com>
2025-09-14 15:20:49 -0400

    Created basic UI for the frontend.

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    return(
+        <div className="page">
+            <a className="nav" href="/new-face">
+                Upload your photo to the database.
+            </a>
+
+            <a className="nav" href="recognize-faces">
+                Use our facial recognition software. 
+            </a>
+         </div>
+    )
+}
+
+export default Home; 
---
+++ frontend/src/Upload.js
@@ -0,0 +1,9 @@
+const Upload = () => {
+    return(
+        <div  className="page">
+            
+        </div>
+    )
+}
+
+export default Upload; 
